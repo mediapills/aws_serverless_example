f4230a04f9ce7c3f4a938031a5e7c37fa36fcf44
mediapills <mediapills@gmail.com>
2022-08-08 15:59:25 +0300

    API GW fixes

--- aws_api_gateway.tf
+++ aws_api_gateway.tf
@@ -18,11 +18,26 @@ resource "aws_api_gateway_rest_api" "main" {
 resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
 
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.main.id,
+      aws_api_gateway_method.main.id,
+      aws_api_gateway_integration.main.id,
+    ]))
+  }
+
   lifecycle {
     create_before_destroy = true
   }
 
-  depends_on = [aws_api_gateway_method.main]
+  depends_on = [aws_api_gateway_method.main, aws_api_gateway_method_response.main]
 }
 
 resource "aws_api_gateway_stage" "main" {
@@ -74,11 +89,15 @@ resource "aws_api_gateway_method_response" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   resource_id = aws_api_gateway_resource.main.id
   http_method = aws_api_gateway_method.main.http_method
-  status_code = "200"
+  status_code = 200
   response_parameters = {
       "method.response.header.Access-Control-Allow-Headers" = true,
       "method.response.header.Access-Control-Allow-Methods" = true,
       "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+     "application/json" = "Empty"
   }
 }
 
